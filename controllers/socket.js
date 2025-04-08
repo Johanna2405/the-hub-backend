75ec3119ba7fc9cd179876244e79be9acbc160a9
Anne Martinez <martinezannemerlyn@gmail.com>
2025-04-08 17:29:52 +0200

    adding online status

--- controllers/socket.js
+++ controllers/socket.js
@@ -3,8 +3,17 @@ import models from "../models/index.js";
 const { Message } = models;
 
 export default function setupSocket(io) {
+  const onlineUsers = {};
+
   io.on("connection", (socket) => {
-    console.log(`User connected: ${socket.id}`);
+    // console.log(`User connected: ${socket.id}`);
+
+    socket.on("user_connected", (user) => {
+      onlineUsers[user.id] = socket.id;
+      console.log("Online users:", Object.keys(onlineUsers));
+
+      io.emit("update_online_users", Object.keys(onlineUsers));
+    });
 
     socket.on("send_message", async (data) => {
       const message = await Message.create({
@@ -16,6 +25,7 @@ export default function setupSocket(io) {
         ...message.dataValues,
         User: data.user,
       };
+
       io.emit("receive_message", fullMessage);
     });
 
@@ -28,6 +38,15 @@ export default function setupSocket(io) {
 
     socket.on("disconnect", () => {
       console.log(`User disconnected: ${socket.id}`);
+
+      const disconnectedUserId = Object.keys(onlineUsers).find(
+        (id) => onlineUsers[id] === socket.id
+      );
+      if (disconnectedUserId) {
+        delete onlineUsers[disconnectedUserId];
+      }
+
+      io.emit("update_online_users", Object.keys(onlineUsers));
     });
   });
 }
