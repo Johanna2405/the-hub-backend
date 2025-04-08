f16553789c057b84dffd6f66715dfb1dd08b8c83
Anne Martinez <martinezannemerlyn@gmail.com>
2025-04-08 16:52:20 +0200

    updated for websocket implementation

---
+++ controllers/socket.js
@@ -0,0 +1,26 @@
+import { sequelize } from "../db/index.js";
+import models from "../models/index.js";
+const { Message } = models;
+
+export default function setupSocket(io) {
+  io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on("send_message", async (data) => {
+      const message = await Message.create({
+        user_id: data.user_id,
+        content: data.content,
+      });
+
+      const fullMessage = {
+        ...message.dataValues,
+        User: data.user,
+      };
+      io.emit("receive_message", fullMessage);
+    });
+
+    socket.on("disconnect", () => {
+      console.log(`User disconnected: ${socket.id}`);
+    });
+  });
+}
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./swagger.js";
 import { connectDB } from "./db/index.js";
+import setupSocket from "./controllers/socket.js";
 
-//importing routes
+// Routes
 import messageRoutes from "./routes/messageRoutes.js";
 import listRoutes from "./routes/listRoutes.js";
 import eventRoutes from "./routes/eventRoutes.js";
@@ -17,15 +20,20 @@ import registerRoutes from "./routes/registerRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js";
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Base route
 app.get("/api/", (req, res) => {
   res.send("API is running...");
 });
 
-//routes
+// Routes
 app.use("/api/login", registerRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/lists", listRoutes);
@@ -36,12 +44,27 @@ app.use("/api/users", userRoutes);
 app.use("/api/communities", communityRoutes);
 app.use("/api/comments", commentRoutes);
 
+// Swagger docs
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+// HTTP server from Express
+const server = http.createServer(app);
+
+// Attach Socket.IO
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+setupSocket(io);
+
+// Start everything
 const startServer = async () => {
   await connectDB();
-  app.listen(PORT, () => {
-    console.log(`server running on port ${PORT} ->  http://localhost:${PORT}/`);
+
+  server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
     console.log(`Swagger docs at http://localhost:${PORT}/api-docs`);
   });
 };
