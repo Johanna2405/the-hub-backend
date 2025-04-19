ff31a3f71eeac0c560d0f9022b8975b17380f3a9
Johanna <johanna.k.1996@googlemail.com>
2025-04-19 17:44:20 +0200

    connect database

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: "../.env" });
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
